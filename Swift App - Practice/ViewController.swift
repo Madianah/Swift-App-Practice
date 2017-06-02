447b3f855f3512bc0f7d11eedb8de8f0e97f798d
Madiana Hedayet <Maddie@Madianas-MacBook-Pro.local>
2017-06-02 15:55:33 -0700

    Initial Commit

---
+++ Swift App - Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App - Practice
+//
+//  Created by Madiana Hedayet on 6/2/17.
+//  Copyright © 2017 Madiana Hedayet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
