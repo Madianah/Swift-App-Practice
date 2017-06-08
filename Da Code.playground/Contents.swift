f8de1ddd4b10298afbf418f114f9667cba8bef30
Madiana Hedayet <madianah@gmail.com>
2017-06-08 16:56:01 -0700

    added stuff

---
+++ Da Code.playground/Contents.swift
@@ -0,0 +1,5 @@
+var age = 27
+if age >=16 {
+print ("You can go on the ride")}
+
+
---
+++ Practice Code.playground/Contents.swift
@@ -0,0 +1,26 @@
+
+/*var age = 27
+
+if age != 16 {
+    print ("You can go on the ride")
+    
+}
+
+var name = "Maddie"
+
+if name == "Maddie" {
+    print ("We have the same name!")}
+    
+else
+    {print ("We dont have the same name :(") } */
+
+var birthyear = 1992
+
+if birthyear <= 2000 {
+    print("You were born in the 20th century!")
+} else {
+    print("You were born in the 21st century")
+}
+
+
+
