bb6057b662af576e9ce12cc73c695b488542ffd7
Madiana Hedayet <madianah@gmail.com>
2017-06-08 17:39:42 -0700

    Editing to include math portion of app - (reference lesson: "TextFields" - Git and GitHub)

---
+++ Math Lesson.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+5 + 8
+
+var a = 6
+var b = 3
+
+a + b
+
+a - b
+
+var c = a * b + 400
+
+100 / 21
+
+var age = 27 //this is an "integer" because it's a full number
+
+var weight = 187.56 //"doubles" are numbers with decimals
+
+100.00 / 21.00
+
+// can't do math with an integer and a double
+
+Double(age) * weight
+
+age * Int(weight)
+
